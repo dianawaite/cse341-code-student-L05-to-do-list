d8435a4d5bccbdb6e298a70d5327e079bf2d41a4
dianawaite <wai19007@byui.edu>
2023-02-05 19:37:17 -0700

    rename json files

--- swagger.js
+++ swagger.js
@@ -3,7 +3,7 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'My API',
-    description: 'Temple API'
+    description: 'To Do API'
   },
   host: 'localhost:3000',
   schemes: ['http']
